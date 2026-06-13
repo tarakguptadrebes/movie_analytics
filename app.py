dd4f642e013d373006cdcdd56966c75301e8bd89
Tarak Gupta Drebes <tarak.guptadrebes@gmail.com>
2026-06-13 01:30:52 +0100

    improve chart formatting

--- app.py
+++ app.py
@@ -28,33 +28,41 @@ def load_genre_revenue_total():
 def load_genre_revenue_trend():
     return pd.read_sql('SELECT * FROM genre_revenue_trend', engine)
 
-
 genres_list = ['Action', 'Adventure', 'Animation', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller']
-
 colors = ['#FF6B6B', '#51CF66', '#FFD43B', '#74C0FC', '#B197FC', '#FFA94D', '#63E6BE', '#F783AC', '#94D82D', '#3BC9DB', '#DA77F2']
-
 genre_color_map = dict(zip(genres_list, colors))
 
-
 df_ratings = load_genre_ratings()
+df_revenue_total = load_genre_revenue_total()
+df_revenue_trend = load_genre_revenue_trend()
+
+selected_genres = st.multiselect(
+    label='Filter Genres',
+    options=genres_list,
+    default=genres_list
+)
+
+df_filtered_ratings = df_ratings[df_ratings['genre'].isin(selected_genres)]
+df_filtered_total = df_revenue_total[df_revenue_total['genre'].isin(selected_genres)]
+df_filtered_trend = df_revenue_trend[df_revenue_trend['genre'].isin(selected_genres)]
+
+dynamic_order = sorted(df_filtered_ratings['genre'].unique().tolist())
 
 fig = px.box(
-    df_ratings,
-    x='genre', 
-    y='avg_rating',
+    df_filtered_ratings, 
+    x='avg_rating',
+    y='genre',
     title='Rating by Genre',
-    labels={'genre': 'Genre', 'avg_rating': 'Rating'},
+    labels={'avg_rating': 'Rating', 'genre': 'Genre'},
     color='genre',
     color_discrete_map=genre_color_map,
-    category_orders={'genre': genres_list}
+    category_orders={'genre': dynamic_order}
 )
 
 st.plotly_chart(fig, width='stretch')
 
-df_revenue_total = load_genre_revenue_total()
-
 fig = px.bar(
-    df_revenue_total,
+    df_filtered_total,
     x='genre',
     y='weighted_revenue_share',
     title='Weighted Revenue Share by Genre',
@@ -63,15 +71,11 @@ fig = px.bar(
     color_discrete_map=genre_color_map
 )
 
-st.plotly_chart(fig, width='stretch')
-
-df_revenue_trend = load_genre_revenue_trend()
-
-selected_genres = st.multiselect(
-    label='Filter Genres',
-    options=genres_list,
-    default=genres_list
+fig.update_xaxes(
+    range=[-0.5, len(genres_list) - 0.5] 
 )
+
+st.plotly_chart(fig, width='stretch')
 
 df_filtered_trend = df_revenue_trend[df_revenue_trend['genre'].isin(selected_genres)]
 
